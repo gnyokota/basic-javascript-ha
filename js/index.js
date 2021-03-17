1bb6fd2992f4588ea7cf21ca434d6e8341080d44
Giovana Yokota <gnyokota@gmail.com>
2021-03-17 20:27:11 +0100

    enter trigger event

--- js/index.js
+++ js/index.js
@@ -4,31 +4,65 @@ const addButton = document.querySelector('.main-add-btn');
 const deleteButton = document.querySelector('.main-delete-btn');
 const todoList = document.querySelector('#todo-list');
 
-
-const createTodo = (text, checked='') => {
-    
-    todoList.innerHTML = `
-    <div class='d-flex w-100 align-items-center justify-content-center'>
-        <input class="form-check-input m-1 item-checkbox" type="checkbox" ${checked}>
-        <li class="rounded-1 w-75 border-0 list-group-item">
-          ${text}
-        </li>
-        <button type="button" class="btn btn-success m-1 px-3">Edit</button>
-        <button type="button" class="btn btn-danger m-1">Delete</button>
-      </div>
-    `
-
-
+//function to create the todo items:
+const createTodo = (text) => {
+    // create the wrapper:
+    const divWrapper = document.createElement("div");
+    divWrapper.setAttribute('class','d-flex w-100 align-items-center justify-content-center');
+    //create checkBox:
+    const checkboxInput = document.createElement('input');
+    checkboxInput.setAttribute('class','form-check-input m-1 item-checkbox'); 
+    checkboxInput.setAttribute('type','checkbox'); 
+    //create li:
+    const liTodo = document.createElement("li");
+    liTodo.setAttribute('class','rounded-1 w-75 border-0 list-group-item');
+    liTodo.innerHTML = text; 
+    //create edit button
+    const editTodoButton = document.createElement("button");
+    editTodoButton.setAttribute('class','btn btn-success m-1 px-3');
+    editTodoButton.innerHTML = 'edit'; 
+    //create delete button
+    const deleteTodoButton = document.createElement("button");
+    deleteTodoButton.setAttribute('class','btn btn-danger m-1');
+    deleteTodoButton.innerHTML ='delete'; 
+    //append elements inside wrapper:
+    divWrapper.appendChild(checkboxInput);
+    divWrapper.appendChild(liTodo);
+    divWrapper.appendChild(editTodoButton);
+    divWrapper.appendChild(deleteTodoButton);
+    //append the wrapper inside the ul:
+    todoList.append(divWrapper);
 }
 
-// 3.Events:
-//add todo
-addButton.addEventListener('click', function(e){
+//function to add todo item:
+const addTodo =()=>{
     //trim is necessary to avoid taking in account the spaces
     if(input.value.trim()){
-        createTodo(target.value);
+        createTodo(input.value);
+        //to empty the input field
+        input.value=''
     }else{
         alert('Please, add a todo first!!')
     }
+}
+
+// function to delete all todos:
+const deleteAll = ()=>{
+    while(todoList.firstChild){
+        todoList.removeChild(todoList.firstChild);
+    }
+}
+
+// 3.Events:
+//add todo clicking:
+addButton.addEventListener('click', addTodo)
+
+//add todo with ENTER
+input.addEventListener('keypress',function(e){ 
+    if(e.keyCode === 13){
+        addTodo();
+    }
 })
 
+//remove all 
+deleteButton.addEventListener('click', deleteAll)
